d1a121259db0d3cd2c115f4ea2af38f0a89df2c4
baron070981 <baron070981@gmail.com>
2022-02-11 00:10:59 +0300

    -----------

--- webpsychics/psychics/forms.py
+++ webpsychics/psychics/forms.py
@@ -4,7 +4,8 @@ from django import forms
 
 
 class SendingNumberForm(forms.Form):
-    number = forms.CharField(label='Введите задуманое число:', max_length=2)
+    number = forms.CharField(label='Введите задуманое число:', 
+                             max_length=2)
 
 
 
